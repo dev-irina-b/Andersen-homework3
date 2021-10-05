4c76645178798e276f222a9374b502bf05356a74
dev-irina-b <dev.irina.b@gmail.com>
2021-10-05 15:44:45 +0300

    Initial commit

---
+++ app/src/main/java/android/example/homework3/SecondActivity.kt
@@ -0,0 +1,63 @@
+package android.example.homework3
+
+import android.graphics.BitmapFactory
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import java.net.URL
+import kotlin.concurrent.thread
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var editText: EditText
+    private lateinit var imageView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        editText = findViewById(R.id.editText)
+        imageView = findViewById(R.id.imageView)
+        editText.imeOptions = EditorInfo.IME_ACTION_DONE
+
+        setUpViews()
+    }
+
+    private fun setUpViews() {
+        editText.setOnEditorActionListener { _, i, _ ->
+            if (i == EditorInfo.IME_ACTION_DONE) {
+                loadImage()
+                editText.clearFocus()
+            }
+            false
+        }
+    }
+
+    private fun loadImage() {
+        thread {
+            try {
+                val url = URL(editText.text.toString())
+                val stream = url.openStream()
+                val result = BitmapFactory.decodeStream(stream)
+                stream.close()
+                runOnUiThread {
+                    try {
+                        imageView.setImageBitmap(result!!)
+                        editText.setText("")
+                    } catch (e: Exception) {
+                        Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
+                        imageView.setImageBitmap(null)
+                    }
+                }
+            } catch (e: Exception) {
+                runOnUiThread {
+                    Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
+                    imageView.setImageBitmap(null)
+                }
+            }
+        }
+    }
+}
